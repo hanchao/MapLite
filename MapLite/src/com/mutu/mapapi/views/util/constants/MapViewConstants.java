28ed23587de38810377dd28552266c47a3a2efb0
hanchao <hanchao0123@hotmail.com>
2013-08-20 15:38:06 +0800

    changed max level

--- MapLite/src/com/mutu/mapapi/views/util/constants/MapViewConstants.java
+++ MapLite/src/com/mutu/mapapi/views/util/constants/MapViewConstants.java
@@ -32,5 +32,5 @@ public interface MapViewConstants {
 	 * Maximum Zoom Level - we use Integers to store zoom levels so overflow happens at 2^32 - 1,
 	 * but we also have a tile size that is typically 2^8, so (32-1)-8-1 = 22
 	 */
-	public static final int MAXIMUM_ZOOMLEVEL = 22;
+	public static final int MAXIMUM_ZOOMLEVEL = 21;
 }
