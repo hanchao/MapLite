b827870ee2aa91175f63d8ac5b72cb70f13bdc7e
hanchao <hanchao0123@hotmail.com>
2013-09-06 17:27:55 +0800

    reduce  Animation time

--- MapLite/src/com/mutu/mapapi/views/util/constants/MapViewConstants.java
+++ MapLite/src/com/mutu/mapapi/views/util/constants/MapViewConstants.java
@@ -21,7 +21,7 @@ public interface MapViewConstants {
 	public static final int ANIMATION_SMOOTHNESS_DEFAULT = 10;
 	public static final int ANIMATION_SMOOTHNESS_HIGH = 20;
 
-	public static final int ANIMATION_DURATION_SHORT = 500;
+	public static final int ANIMATION_DURATION_SHORT = 300;
 	public static final int ANIMATION_DURATION_DEFAULT = 1000;
 	public static final int ANIMATION_DURATION_LONG = 2000;
 
